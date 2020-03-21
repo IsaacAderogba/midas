286a86f2eeaed99d9b0b596e86d34e286c590e27
Isaac Aderogba <isaacaderogba1@gmail.com>
2020-03-21 15:19:41 +0000

    Setup staging and production envs for knex

--- knexfile.js
+++ knexfile.js
@@ -11,5 +11,37 @@ module.exports = {
       directory: __dirname + "/db/seeds"
     },
     useNullAsDefault: true
+  },
+  staging: {
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: __dirname + "/db/migrations",
+      tableName: "db_migrations"
+    },
+    seeds: {
+      directory: __dirname + "/db/seeds"
+    },
+    useNullAsDefault: true
+  },
+  production: {
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: __dirname + "/db/migrations",
+      tableName: "db_migrations"
+    },
+    seeds: {
+      directory: __dirname + "/db/seeds"
+    },
+    useNullAsDefault: true
   }
 };
