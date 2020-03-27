9e9fa404347da26b85e0d46946662b6d4d4228b2
Isaac Aderogba <isaacaderogba1@gmail.com>
2020-03-27 13:23:41 +0000

    Migrate away from apollo-boost to allow sub support

--- client/src/~reusables/contexts/ApolloContextProvider.tsx
+++ client/src/~reusables/contexts/ApolloContextProvider.tsx
@@ -1,9 +1,40 @@
 import React from "react";
-import ApolloClient from "apollo-boost";
+import { ApolloClient } from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { HttpLink } from "apollo-link-http";
+import { onError } from "apollo-link-error";
+import { ApolloLink } from "apollo-link";
+import { setContext } from "apollo-link-context";
 import { ApolloProvider } from "react-apollo";
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? token : ""
+    }
+  };
+});
+
 const client = new ApolloClient({
-  uri: "/graphql"
+  link: ApolloLink.from([
+    onError(({ graphQLErrors, networkError }) => {
+      if (graphQLErrors)
+        graphQLErrors.forEach(({ message, locations, path }) =>
+          console.log(
+            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+          )
+        );
+      if (networkError) console.log(`[Network error]: ${networkError}`);
+    }),
+    authLink,
+    new HttpLink({
+      uri: "/graphql",
+      credentials: "same-origin"
+    })
+  ]),
+  cache: new InMemoryCache()
 });
 
 export const ApolloContextProvider: React.FC = ({ children }) => {
