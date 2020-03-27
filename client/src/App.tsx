3a727939f74e0f4992dffdf5ac9d263b48a1c7f0
Isaac Aderogba <isaacaderogba1@gmail.com>
2020-03-27 18:07:51 +0000

    Scaffold landing page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,19 @@
+// modules
 import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import { Landing } from './views/marketing/Landing';
+
+// components
+
+// helpers
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        <Route exact path="/" render={routeProps => <Landing {...routeProps} />} />
+        <Redirect to="/" />
+      </Switch>
     </div>
   );
 }
---
+++ client/src/views/marketing/Landing.tsx
@@ -0,0 +1,11 @@
+// modules
+import React from 'react';
+import { RouteComponentProps } from 'react-router-dom';
+
+export const Landing: React.FC<RouteComponentProps> = () => {
+  return (
+    <div>
+      landing
+    </div>
+  )
+}
