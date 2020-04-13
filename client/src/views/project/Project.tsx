056c6f3ffac7fed2778a45f01fe115ab98b81f8c
Isaac Aderogba <isaacaderogba1@gmail.com>
2020-04-13 16:21:33 +0100

    Create useprojectsubscription callback

--- client/src/views/project/Project.tsx
+++ client/src/views/project/Project.tsx
@@ -8,12 +8,40 @@ import { CustomizeSidebar } from "./CustomizeSidebar";
 import { StatefulCanvas } from "./Canvas";
 
 // helpers
-import { useProjectSubscription } from "../../generated/graphql";
+import { useProjectSubscription, CanvasScene } from "../../generated/graphql";
 import { useProjectStore } from "../../~reusables/contexts/ProjectProvider";
 
 export const Project: React.FC = () => {
   const project = useProjectStore(state => state.project);
-  console.log(project)
+  useProjectSubscription({
+    variables: { where: { id: project?.id } },
+    onSubscriptionData({ subscriptionData: { data: subData } }) {
+      if (subData && subData.project && subData.project.canvasPayload) {
+        const { data, mutation, canvasPayload } = subData.project;
+        const {
+          pointerCoordX,
+          pointerCoordY,
+          canvasScene,
+          workspaceUserId
+        } = canvasPayload;
+
+        switch (canvasScene) {
+          case CanvasScene.ClientConnect:
+            break;
+          case CanvasScene.ClientDisconnect:
+            break;
+          case CanvasScene.MouseLocation:
+            break;
+          case CanvasScene.SceneUpdate:
+            break;
+          default:
+            break;
+        }
+      }
+    }
+  });
+
+  console.log(project);
   return (
     <section>
       <CanvasTopbar />
