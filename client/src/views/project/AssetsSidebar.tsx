495fe466e368004ad3e33a69a7d70e6861e6d1bd
Isaac Aderogba <isaacaderogba1@gmail.com>
2020-04-14 20:56:20 +0100

    UI for colours

--- client/src/views/project/AssetsSidebar.tsx
+++ client/src/views/project/AssetsSidebar.tsx
@@ -1,9 +1,90 @@
-// modules 
-import React from 'react';
+// modules
+import React from "react";
+import { useContextSelector } from "use-context-selector";
+import { css } from "styled-components/macro";
 
 // components
-import { CanvasSidebar } from '../../components/~layout/CanvasSidebar';
+import { CanvasSidebar } from "../../components/~layout/CanvasSidebar";
+
+// helpers
+import { CanvasContext } from "../../~reusables/contexts/CanvasProvider";
+import { P2 } from "../../components/atoms/Text";
 
 export const AssetsSidebar: React.FC = () => {
-  return <CanvasSidebar align="left">AssetsSidebar</CanvasSidebar>
-}
+  const {
+    setCanvasState,
+    viewBackgroundColor,
+    currentItemStrokeColor,
+    currentItemBackgroundColor
+  } = useContextSelector(CanvasContext, state => ({
+    setCanvasState: state.setCanvasState,
+    viewBackgroundColor: state.viewBackgroundColor,
+    currentItemStrokeColor: state.currentItemStrokeColor,
+    currentItemBackgroundColor: state.currentItemBackgroundColor
+  }));
+
+  return (
+    <CanvasSidebar align="left">
+      <ColorPanel
+        title="Background"
+        value={viewBackgroundColor}
+        onChange={e => {
+          setCanvasState(prevState => ({
+            ...prevState,
+            viewBackgroundColor: e.target.value
+          }));
+        }}
+      />
+      <ColorPanel
+        title="Shape Stroke"
+        value={currentItemStrokeColor}
+        onChange={e => {
+          setCanvasState(prevState => ({
+            ...prevState,
+            currentItemStrokeColor: e.target.value
+          }));
+        }}
+      />
+      <ColorPanel
+        title="Shape Background"
+        value={currentItemBackgroundColor}
+        onChange={e => {
+          setCanvasState(prevState => ({
+            ...prevState,
+            currentItemBackgroundColor: e.target.value
+          }));
+        }}
+      />
+    </CanvasSidebar>
+  );
+};
+
+const ColorPanel = ({
+  title,
+  value,
+  onChange
+}: {
+  title: string;
+  value: string;
+  onChange: ((event: React.ChangeEvent<HTMLInputElement>) => void) | undefined;
+}) => {
+  return (
+    <section
+      css={css`
+        border-bottom: 1px solid ${p => p.theme.colors.greys[8]};
+        padding: ${p => p.theme.space[6]}px;
+      `}
+    >
+      <div>
+        <P2
+          css={css`
+            margin-bottom: ${p => p.theme.space[3]}px;
+          `}
+        >
+          {title}
+        </P2>
+      </div>
+      <input type="color" value={value} onChange={onChange} />
+    </section>
+  );
+};
--- client/src/views/project/CustomizeSidebar.tsx
+++ client/src/views/project/CustomizeSidebar.tsx
@@ -22,122 +22,64 @@ import { useProjectStore } from "../../~reusables/contexts/ProjectProvider";
 
 export const CustomizeSidebar: React.FC = () => {
   const elements = useProjectStore(state => state.elements);
-  const {
-    setCanvasState,
-    viewBackgroundColor,
-    currentItemStrokeColor,
-    currentItemBackgroundColor,
-    forceCanvasUpdate
-  } = useContextSelector(CanvasContext, state => ({
-    setCanvasState: state.setCanvasState,
-    viewBackgroundColor: state.viewBackgroundColor,
-    currentItemStrokeColor: state.currentItemStrokeColor,
-    currentItemBackgroundColor: state.currentItemBackgroundColor,
+  const { forceCanvasUpdate } = useContextSelector(CanvasContext, state => ({
     forceCanvasUpdate: state.forceCanvasUpdate
   }));
 
   return (
     <CanvasSidebar align="right">
-      <div className="panelTools"></div>
-      <h4>Colors</h4>
-      <div className="panelColumn">
-        <label>
-          <input
-            type="color"
-            value={viewBackgroundColor}
-            onChange={e => {
-              setCanvasState(prevState => ({
-                ...prevState,
-                viewBackgroundColor: e.target.value
-              }));
-            }}
-          />
-          Background
-        </label>
-        <label>
-          <input
-            type="color"
-            value={currentItemStrokeColor}
-            onChange={e => {
-              setCanvasState(prevState => ({
-                ...prevState,
-                currentItemStrokeColor: e.target.value
-              }));
-            }}
-          />
-          Shape Stroke
-        </label>
-        <label>
-          <input
-            type="color"
-            value={currentItemBackgroundColor}
-            onChange={e => {
-              setCanvasState(prevState => ({
-                ...prevState,
-                currentItemBackgroundColor: e.target.value
-              }));
-            }}
-          />
-          Shape Background
-        </label>
-      </div>
       {someElementIsSelected(elements) && (
-        <>
-          <h4>Shape options</h4>
-          <div className="panelColumn">
-            <button
-              onClick={() =>
-                deleteSelectedElements(elements, forceCanvasUpdate)
-              }
-            >
-              Delete
-            </button>
-            <button
-              onClick={() =>
-                moveOneRight(
-                  elements,
-                  getSelectedIndices(elements),
-                  forceCanvasUpdate
-                )
-              }
-            >
-              Bring forward
-            </button>
-            <button
-              onClick={() =>
-                moveAllRight(
-                  elements,
-                  getSelectedIndices(elements),
-                  forceCanvasUpdate
-                )
-              }
-            >
-              Bring to front
-            </button>
-            <button
-              onClick={() =>
-                moveOneLeft(
-                  elements,
-                  getSelectedIndices(elements),
-                  forceCanvasUpdate
-                )
-              }
-            >
-              Send backward
-            </button>
-            <button
-              onClick={() => {
-                moveAllLeft(
-                  elements,
-                  getSelectedIndices(elements),
-                  forceCanvasUpdate
-                );
-              }}
-            >
-              Send to back
-            </button>
-          </div>
-        </>
+        <div className="panelColumn">
+          <button
+            onClick={() => deleteSelectedElements(elements, forceCanvasUpdate)}
+          >
+            Delete
+          </button>
+          <button
+            onClick={() =>
+              moveOneRight(
+                elements,
+                getSelectedIndices(elements),
+                forceCanvasUpdate
+              )
+            }
+          >
+            Bring forward
+          </button>
+          <button
+            onClick={() =>
+              moveAllRight(
+                elements,
+                getSelectedIndices(elements),
+                forceCanvasUpdate
+              )
+            }
+          >
+            Bring to front
+          </button>
+          <button
+            onClick={() =>
+              moveOneLeft(
+                elements,
+                getSelectedIndices(elements),
+                forceCanvasUpdate
+              )
+            }
+          >
+            Send backward
+          </button>
+          <button
+            onClick={() => {
+              moveAllLeft(
+                elements,
+                getSelectedIndices(elements),
+                forceCanvasUpdate
+              );
+            }}
+          >
+            Send to back
+          </button>
+        </div>
       )}
     </CanvasSidebar>
   );
