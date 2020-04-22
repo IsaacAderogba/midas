9cdf8e0ea83117bd231512b5c31e284575a27fba
Isaac Aderogba <isaacaderogba1@gmail.com>
2020-04-22 12:50:25 +0100

    Set up mac, windows and linux dev builds

--- main.js
+++ main.js
@@ -1,15 +1,58 @@
-const { app, BrowserWindow } = require("electron");
+require("dotenv").config();
+const { app, BrowserWindow, dialog } = require("electron");
+const { autoUpdater } = require("electron-updater");
+
+autoUpdater.logger = require("electron-log");
+autoUpdater.logger.transports.file.level = "info";
+autoUpdater.autoDownload = false;
+
+const updater = () => {
+  autoUpdater.checkForUpdates();
+
+  autoUpdater.on("update-available", () => {
+    dialog.showMessageBox(
+      {
+        type: "info",
+        title: "Update available",
+        message:
+          "A new version of Midas is available. Do you want to update now?",
+        buttons: ["Update", "Later"],
+      },
+      (buttonIndex) => {
+        if (buttonIndex === 0) {
+          autoUpdater.downloadUpdate();
+        }
+      }
+    );
+  });
+
+  autoUpdater.on("update-downloaded", () => {
+    dialog.showMessageBox(
+      {
+        type: "info",
+        title: "Update ready",
+        message: "Install and restart now?",
+        buttons: ["Yes", "Later"],
+      },
+      (buttonIndex) => {
+        if (buttonIndex === 0) autoUpdater.quitAndInstall(false, true);
+      }
+    );
+  });
+};
 
 let mainWindow;
 
 function createWindow() {
+  updater();
+
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
-    webPreferences: { nodeIntegration: true }
+    webPreferences: { nodeIntegration: true },
   });
 
-  mainWindow.loadURL(process.env.DOMAIN);
+  mainWindow.loadURL(process.env.DOMAIN || "https://getmidas.herokuapp.com");
 
   // Listen for window being closed
   mainWindow.on("closed", () => {
